4d667446b01f7beb8999eb8a2f0986ce3be8ef31
UndrcoverCactus <meye3224@umn.edu>
2026-05-02 16:15:46 -0500

    Add TestTurnOrder, TestJumpCastLimit, and TestJumpOnOwnPieces

--- test_spell_logic.py
+++ test_spell_logic.py
@@ -43,3 +43,94 @@ class TestNewGameResetsBoard:
 #  Write tests that check the rules from SPELL_CHESS_RULES.md.        #
 #  If a test fails, you've found a bug — document it!                 #
 # ------------------------------------------------------------------ #
+
+class TestTurnOrder:
+    """The game should start with white's turn, then switch to black, then back to white, etc. after each move"""
+
+    def test_white_turn_starts(self):
+        game = SpellChessGame()
+        assert game.current_turn() == chess.WHITE
+
+    def test_black_turn_second(self):
+        game = SpellChessGame()
+        game.make_move(chess.E2, chess.E3)
+        assert game.current_turn() == chess.BLACK
+
+    def test_white_turn_third(self):
+        game = SpellChessGame()
+        game.make_move(chess.E2, chess.E3)
+        game.make_move(chess.E7, chess.E6)
+        assert game.current_turn() == chess.WHITE
+
+class TestJumpCastLimit:
+    """The Jump spell should only be able to be cast once per turn"""
+
+    def test_jump_updates_casted_flag(self):
+        game = SpellChessGame()
+        # White casts jump on pawn
+        game.cast_jump(chess.E2, chess.D4)
+        # game.jump_casted_this_turn should now be True
+        assert game.jump_casted_this_turn
+
+    def test_jump_cant_cast_twice(self):
+        game = SpellChessGame()
+        # White casts jump on rook
+        game.cast_jump(chess.A1, chess.A3)
+        # White should not be able to cast jump again
+        assert not game.cast_jump(chess.E2, chess.D4)
+
+    def test_jump_flag_resets_next_turn(self):
+        game = SpellChessGame()
+
+        # White Turn - Jump + Move
+        game.cast_jump(chess.G2, chess.F4)
+        game.make_move(chess.E2, chess.E4)
+
+        # Black Turn - is able to jump
+        assert not game.jump_casted_this_turn
+
+    def test_jump_can_cast_next_turn(self):
+        game = SpellChessGame()
+
+        # White Turn - Jump + Move
+        game.cast_jump(chess.G2, chess.F4)
+        game.make_move(chess.E2, chess.E4)
+
+        # Black Turn - is able to jump
+        assert game.cast_jump(chess.A8, chess.B6)
+
+class TestJumpOnOwnPieces:
+    """The Jump spell should only be able to be cast on one's own pieces"""
+
+    def test_white_jump_casts_on_white_piece(self):
+        game = SpellChessGame()
+
+        # White Turn - cast jump on white piece
+        assert game.cast_jump(chess.A1, chess.B2)
+
+    def test_white_jump_cant_cast_on_black_piece(self):
+        game = SpellChessGame()
+
+        # White Turn - cast jump on black piece
+        assert not game.cast_jump(chess.C8, chess.C6)
+
+    def test_black_jump_casts_on_black_piece(self):
+        game = SpellChessGame()
+
+        # Write Turn - move piece
+        game.make_move(chess.E2, chess.E4)
+
+        # Black Turn - cast jump on black piece
+        assert game.cast_jump(chess.H8, chess.H6)
+
+    def test_black_jump_cant_cast_on_white_piece(self):
+        game = SpellChessGame()
+
+        # Write Turn - move piece
+        game.make_move(chess.E2, chess.E4)
+
+        # Black Turn - cast jump on white piece
+        assert not game.cast_jump(chess.C8, chess.C6)
+
+
+
