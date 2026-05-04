a7c170ee0e22ca2c25b9599ce965a789b05e43fa
Rohan <ramma006@umn.edu>
2026-05-04 01:07:42 -0500

    Testing for Checks,Checkmate,Draws,En Passant, Promotion

--- test_spell_logic.py
+++ test_spell_logic.py
@@ -195,3 +195,174 @@ class TestVisualDisplay:
         freeze_info = game.freeze_info_text()
         assert game.freeze_effect_color == None
         assert "— pieces in area are frozen" not in freeze_info 
+        
+class TestCheckRules:
+    def test_player_cannot_move_into_check(self):
+        game = SpellChessGame()
+        king = game.board.remove_piece_at(chess.E1)
+        game.board.set_piece_at(chess.E5,king)
+        #If King moves to E6 it goes into check
+        result = game.make_move(chess.E5,chess.E6)
+        assert result == False
+        
+    def test_player_cannot_ignore_check(self):
+        game = SpellChessGame()
+        king = game.board.remove_piece_at(chess.E1)
+        game.board.set_piece_at(chess.E6,king)
+        #King must move out of check
+        result = game.make_move(chess.D2,chess.D4)
+        assert result == False
+        
+    def test_checkmate_ends_game(self):
+        game = SpellChessGame()
+        game.board.remove_piece_at(chess.E2)
+        game.board.remove_piece_at(chess.G7)
+        game.board.remove_piece_at(chess.E1)
+        #Have to manually set up fool's mate because make move doesn't wrok
+        assert game.make_move(chess.D1,chess.H5) == True
+        assert game.is_game_over() == True
+        
+    def test_player_cannot_reveal_check(self):
+        game = SpellChessGame()
+        b_queen = game.board.remove_piece_at(chess.D8)
+        game.board.set_piece_at(chess.H4,b_queen)
+        result = game.make_move(chess.F2,chess.F3)
+        assert result == False
+        
+class TestDrawRules:
+    def test_stalemate_conditions(self):
+        game = SpellChessGame()
+        #Remove every piece except the King
+        game.board.remove_piece_at(chess.A2)
+        game.board.remove_piece_at(chess.B2)
+        game.board.remove_piece_at(chess.C2)
+        game.board.remove_piece_at(chess.D2)
+        game.board.remove_piece_at(chess.E2)
+        game.board.remove_piece_at(chess.F2)
+        game.board.remove_piece_at(chess.G2)
+        game.board.remove_piece_at(chess.H2)
+        game.board.remove_piece_at(chess.A1)
+        game.board.remove_piece_at(chess.B1)
+        game.board.remove_piece_at(chess.C1)
+        game.board.remove_piece_at(chess.D1)
+        game.board.remove_piece_at(chess.F1)
+        game.board.remove_piece_at(chess.G1)
+        game.board.remove_piece_at(chess.H1)
+        #Set up position where King can't move
+        queen = game.board.remove_piece_at(chess.D8)
+        game.board.set_piece_at(chess.D6,queen)
+        rook_1 = game.board.remove_piece_at(chess.A8)
+        game.board.set_piece_at(chess.F6,rook_1)
+        rook_2 = game.board.remove_piece_at(chess.H8)
+        game.board.set_piece_at(chess.H2,rook_2)
+        
+        assert game.is_game_over() == True
+        
+    def test_no_mating_material(self):
+        game = SpellChessGame()
+        #Remove every piece except both Kings
+        game.board.remove_piece_at(chess.A2)
+        game.board.remove_piece_at(chess.B2)
+        game.board.remove_piece_at(chess.C2)
+        game.board.remove_piece_at(chess.D2)
+        game.board.remove_piece_at(chess.E2)
+        game.board.remove_piece_at(chess.F2)
+        game.board.remove_piece_at(chess.G2)
+        game.board.remove_piece_at(chess.H2)
+        game.board.remove_piece_at(chess.A1)
+        game.board.remove_piece_at(chess.B1)
+        game.board.remove_piece_at(chess.C1)
+        game.board.remove_piece_at(chess.D1)
+        game.board.remove_piece_at(chess.F1)
+        game.board.remove_piece_at(chess.G1)
+        game.board.remove_piece_at(chess.H1)
+        
+        game.board.remove_piece_at(chess.A7)
+        game.board.remove_piece_at(chess.B7)
+        game.board.remove_piece_at(chess.C7)
+        game.board.remove_piece_at(chess.D7)
+        game.board.remove_piece_at(chess.E7)
+        game.board.remove_piece_at(chess.F7)
+        game.board.remove_piece_at(chess.G7)
+        game.board.remove_piece_at(chess.H7)
+        game.board.remove_piece_at(chess.A8)
+        game.board.remove_piece_at(chess.B8)
+        game.board.remove_piece_at(chess.C8)
+        game.board.remove_piece_at(chess.D8)
+        game.board.remove_piece_at(chess.F8)
+        game.board.remove_piece_at(chess.G8)
+        game.board.remove_piece_at(chess.H8)
+        
+        assert game.is_game_over() == True
+    
+class TestEnPassant:
+    def test_en_passant_legal(self):
+        game = SpellChessGame()
+        # Move white pawn into correct position
+        w_pawn = game.board.remove_piece_at(chess.H1)
+        game.board.set_piece_at(chess.H5,w_pawn)
+        game.board.turn = chess.BLACK
+        assert game.make_move(chess.G7,chess.G5) == True #Set up en-passant
+        game.board.turn = chess.WHITE
+        assert game.make_move(chess.H5,chess.G6) == True #See if en-passant is successful
+        
+    
+    def test_one_turn_limit(self):
+        game = SpellChessGame()
+        # Move white pawn into correct position
+        w_pawn = game.board.remove_piece_at(chess.H1)
+        game.board.set_piece_at(chess.H5,w_pawn)
+        game.board.turn = chess.BLACK
+        assert game.make_move(chess.G7,chess.G5) == True #Set up en-passant
+        game.board.turn = chess.WHITE
+        assert game.make_move(chess.E2,chess.E4) == True
+        game.board.turn = chess.BLACK
+        assert game.make_move(chess.E7,chess.E5) == True
+        game.board.turn = chess.WHITE
+        assert game.make_move(chess.H5,chess.G6) == False #See if en-passant is successful after the window has passed
+        
+    
+class TestPromotion:
+    def test_promotion_default(self):
+        game = SpellChessGame()
+        game.board.remove_piece_at(chess.H8)
+        game.board.remove_piece_at(chess.H7)
+        pawn = game.board.remove_piece_at(chess.H2)
+        game.board.set_piece_at(chess.H7,pawn)
+        assert game.make_move(chess.H7,chess.H8) == True #Does promotion go through
+    def test_promoting_to_queen(self):
+        game = SpellChessGame()
+        game.board.remove_piece_at(chess.H8)
+        game.board.remove_piece_at(chess.H7)
+        pawn = game.board.remove_piece_at(chess.H2)
+        game.board.set_piece_at(chess.H7,pawn)
+        game.make_move(chess.H7,chess.H8,chess.QUEEN)
+        piece = game.board.remove_piece_at(chess.H8)
+        assert piece.piece_type == chess.QUEEN #Did pawn promote to queen?
+    def test_promoting_to_bishop(self):
+        game = SpellChessGame()
+        game.board.remove_piece_at(chess.H8)
+        game.board.remove_piece_at(chess.H7)
+        pawn = game.board.remove_piece_at(chess.H2)
+        game.board.set_piece_at(chess.H7,pawn)
+        game.make_move(chess.H7,chess.H8,chess.BISHOP)
+        piece = game.board.remove_piece_at(chess.H8)
+        assert piece.piece_type == chess.BISHOP #Did pawn promote to bishop?
+    def test_promoting_to_knight(self):
+        game = SpellChessGame()
+        game.board.remove_piece_at(chess.H8)
+        game.board.remove_piece_at(chess.H7)
+        pawn = game.board.remove_piece_at(chess.H2)
+        game.board.set_piece_at(chess.H7,pawn)
+        game.make_move(chess.H7,chess.H8,chess.KNIGHT)
+        piece = game.board.remove_piece_at(chess.H8)
+        assert piece.piece_type == chess.KNIGHT #Did pawn promote to knight?
+    def test_promoting_to_rook(self):
+        game = SpellChessGame()
+        game.board.remove_piece_at(chess.H8)
+        game.board.remove_piece_at(chess.H7)
+        pawn = game.board.remove_piece_at(chess.H2)
+        game.board.set_piece_at(chess.H7,pawn)
+        game.make_move(chess.H7,chess.H8,chess.ROOK)
+        piece = game.board.remove_piece_at(chess.H8)
+        assert piece.piece_type == chess.ROOK #Did pawn promote to queen?
